b59d11305ddaf9871179e0a4ffb8afbc56c81388
egevorgyan <edgar.gevorgyan@student.uclouvain.be>
2019-09-28 15:20:49 +0200

    suppression espace

---
+++ Assignment1/src/main.py
@@ -0,0 +1,9 @@
+
+def print_chess(chess):
+    """
+    This function will print on the standard output the chess, with all
+    the already visited tiles, according to the statement wishes
+    :param chess: the chess we want to print on the standard output
+    :return: nothing
+    """
+
