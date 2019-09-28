3033aa329b1800c086b7cbfa78e2f024bc2f5eef
louisna <louisnavarre@hotmail.com>
2019-09-28 13:14:28 +0200

    Add files

---
+++ Assignment 1/knight.py
@@ -0,0 +1,108 @@
+# -*-coding: utf-8 -*
+'''NAMES OF THE AUTHOR(S): Gael Aglin <gael.aglin@uclouvain.be>'''
+import time
+import sys
+from search import *
+
+
+#################
+# Problem class #
+#################
+class Knight(Problem):
+
+    def successor(self, state):
+        pass
+
+    def goal_test(self, state):
+        pass
+
+
+###############
+# State class #
+###############
+
+class State:
+    def __init__(self, shape, init_pos):
+        self.nCols = shape[0]
+        self.nRows = shape[1]
+        self.grid = []
+        for i in range(self.nRows):
+            self.grid.append([" "] * self.nCols)
+        self.grid[init_pos[0]][init_pos[1]] = "♘"
+
+    def __str__(self):
+        nsharp = (2 * self.nCols) + (self.nCols // 5)
+        s = "#" * nsharp
+        s += "\n"
+        for i in range(self.nRows):
+            s = s + "#"
+            for j in range(self.nCols):
+                s = s + str(self.grid[i][j]) + " "
+            s = s[:-1]
+            s = s + "#"
+            if i < self.nRows - 1:
+                s = s + '\n'
+        s += "\n"
+        s += "#" * nsharp
+        return s
+
+
+##############################
+# Launch the search in local #
+##############################
+#Use this block to test your code in local
+# Comment it and uncomment the next one if you want to submit your code on INGInious
+with open('instancesFile') as f:
+    instances = f.read().splitlines()
+
+for instance in instances:
+    elts = instance.split(" ")
+    shape = (int(elts[0]), int(elts[1]))
+    init_pos = (int(elts[2]), int(elts[3]))
+    init_state = State(shape, init_pos)
+
+    problem = Knight(init_state)
+
+    # example of bfs graph search
+    startTime = time.perf_counter()
+    node, nbExploredNodes = breadth_first_graph_search(problem)
+    endTime = time.perf_counter()
+
+    # example of print
+    path = node.path()
+    path.reverse()
+
+    print('Number of moves: ' + str(node.depth))
+    for n in path:
+        print(n.state)  # assuming that the __str__ function of state outputs the correct format
+        print()
+    print("nb nodes explored = ",nbExploredNodes)
+    print("time : " + str(endTime - startTime))
+
+
+
+####################################
+# Launch the search for INGInious  #
+####################################
+#Use this block to test your code on INGInious
+shape = (int(sys.argv[1]),int(sys.argv[2]))
+init_pos = (int(sys.argv[3]),int(sys.argv[4]))
+init_state = State(shape, init_pos)
+
+problem = Knight(init_state)
+
+# example of bfs graph search
+startTime = time.perf_counter()
+node, nbExploredNodes = breadth_first_graph_search(problem)
+endTime = time.perf_counter()
+
+# example of print
+path = node.path()
+path.reverse()
+
+print('Number of moves: ' + str(node.depth))
+for n in path:
+    print(n.state)  # assuming that the __str__ function of state outputs the correct format
+    print()
+print("nb nodes explored = ",nbExploredNodes)
+print("time : " + str(endTime - startTime))
