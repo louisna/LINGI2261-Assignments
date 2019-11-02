5418861a705537f5b432e2375a61e3f60c4cf2d9
egevorgyan <edgar.gevorgyan@student.uclouvain.be>
2019-11-02 16:16:52 +0100

    basic agent

--- Assignment3/template_agent.py
+++ Assignment3/template_agent.py
@@ -7,7 +7,7 @@ Agent skeleton. Fill in the gaps.
 class MyAgent(AlphaBetaAgent):
 
   """
-  This is the skeleton of an agent to play the Tak game.
+  This is the skeleton of an agent to play the Squadro game.
   """
   def get_action(self, state, last_action, time_left):
     self.last_action = last_action
@@ -20,18 +20,28 @@ class MyAgent(AlphaBetaAgent):
   state s.
   """
   def successors(self, state):
-    pass
+    actions = state.get_current_player_actions()
+    for a in actions:
+      new_state = state.copy()
+      new_state.apply_action(a)
+      yield a,new_state
 
   """
   The cutoff function returns true if the alpha-beta/minimax
   search has to stop and false otherwise.
   """
   def cutoff(self, state, depth):
-    pass
+    if(state.game_over_check()):
+      return True
+    return depth > 2 #arbitrary set max depth to 1
 
   """
   The evaluate function must return an integer value
   representing the utility function of the board.
   """
   def evaluate(self, state):
-    pass
+    sum = 0
+    for pawn in range(5): # they are 5 pawn
+      sum += state.get_pawn_advancement(self.id,pawn)
+    return sum
+
