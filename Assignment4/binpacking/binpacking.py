dfb7b35c49bda2d85fef9f4c64a045d66aebd102
louisna <louisnavarre@hotmail.com>
2019-11-29 23:56:53 +0100

    Corrections and work on INGInious

--- Assignment4/binpacking/binpacking.py
+++ Assignment4/binpacking/binpacking.py
@@ -10,14 +10,40 @@ class BinPacking(Problem):
         # First the successors using the move method
         for i, binA in enumerate(state.bins):
             for j, binB in enumerate(state.bins):
-                if binA != binB:
+                if i != j:
                     for item, weight in binA.items():
+                        #print(binB)
+                        #print("passage", sum(list(binB.values())), state.capacity, weight)
+
                         if state.can_fit(binB, int(weight)):
-                            print(item, binB)
+                            #print('---------------')
+                            #print(item, j, binB, weight)
+                            #print('ici')
+                            #print(item, binB)
                             newstate = state.copy_state()
                             newstate.bins[i].pop(item)
                             newstate.bins[j][item] = weight
-                            yield None, newstate
+                            #print(newstate.bins[i], newstate.bins[j])
+                            if len(newstate.bins[i]) == 0:
+                                del newstate.bins[i]
+                            #print(len(newstate.bins))
+                            yield (item, j), newstate
+        # Second method using swap
+        for i, binA in enumerate(state.bins):
+            for j, binB in enumerate(state.bins):
+                if i < j:
+                    for itemA, weightA in binA.items():
+                        for itemB, weightB in binB.items():
+                            if weightA != weightB:
+                                new_state = state.copy_state()
+                                new_state.bins[i].pop(itemA)
+                                new_state.bins[j].pop(itemB)
+                                if new_state.can_fit(new_state.bins[i], int(weightB)) and new_state.can_fit(new_state.bins[j], int(weightA)):
+                                    new_state.bins[i][itemB] = weightB
+                                    new_state.bins[j][itemA] = weightA
+                                    #print('test debut', new_state.bins[i].keys(), itemA)
+                                    #print("swap", new_state.bins)
+                                    yield None, new_state
 
     def fitness(self, state):
         """
@@ -27,11 +53,13 @@ class BinPacking(Problem):
         fitness = 0
         k = len(state.bins)
         for bin in state.bins:
-            capacity_i = 0
-            for _, weight in enumerate(bin):
-                capacity_i += (int(weight) / state.capacity) ** 2
-            fitness -= capacity_i / k
-        return 1 + fitness
+            fullness = 0
+            for weight in bin.values():
+                fullness += int(weight)
+            fitness += (fullness / state.capacity) ** 2
+            #print("kkk", fullness/state.capacity, state.capacity)
+        #print(1 - fitness / k)
+        return 1 - fitness / k
 
 
 class State:
@@ -86,15 +114,14 @@ def maxvalue(problem, limit=100, callback=None):
         if callback is not None:
             callback(current)
         # Choose the best node from successors
-        possible_moves = list(current.expand())
+        possible_moves = list(best.expand())
         if possible_moves is None or len(possible_moves) == 0:
-            print("ERROR !!!!!!!!")
-            return
-        best_next = possible_moves[0]
-        best_value = problem.fitness(best_next.state)
+            return best
+        best_next = None
+        best_value = math.inf
         for next in possible_moves:
             value = problem.fitness(next.state)
-            if value > best_value:
+            if value < best_value:
                 best_next = next
                 best_value = value
         best = best_next
@@ -109,18 +136,17 @@ def randomized_maxvalue(problem, limit=100, callback=None):
         if callback is not None:
             callback(current)
         # Choose the best node from successors
-        possible_moves = list(current.expand())
-        five_best = [(-math.inf, None)] * 5
+        possible_moves = list(best.expand())
+        five_best = [(math.inf, None)] * 5
         if possible_moves is None or len(possible_moves) == 0:
-            print("ERROR !!!!!!!!")
-            return
+            #print("ONLY ONE")
+            return best
         for next in possible_moves:
             value = problem.fitness(next.state)
-            if value > five_best[0][0]:
+            if value < five_best[0][0]:
                 five_best[0] = (value, next)
-                five_best.sort(key=lambda x: x[0])
+                five_best.sort(key=lambda x: x[0], reverse=True)
         best = random.choice(five_best)[1]
-
     return best
 
 
@@ -133,12 +159,13 @@ if __name__ == '__main__':
     init_state = State(info[0], info[1])
     bp_problem = BinPacking(init_state)
     bp_problem.successor(init_state)
-    step_limit = 300
+    step_limit = 100
     #node = randomized_maxvalue(bp_problem, step_limit)
     node = maxvalue(bp_problem, step_limit)
     state = node.state
-    print(bp_problem.fitness(state))
+    #print(bp_problem.fitness(state))
     print(state)
-    print(init_state)
+    #print(init_state)
+    #sum = 0
     #print(maxvalue(bp_problem).state)
     #print(randomized_maxvalue(bp_problem).state)
